1e47cb52da80cd1d46f6c0e9c14e8212f9b9f76b
Jeroenouw <jeroenouw@hotmail.com>
2017-05-04 11:38:21 +0200

    Db, models, DataAnnotations and migrations

--- EcommerceApplication/Models/Product.cs
+++ EcommerceApplication/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,10 @@ namespace EcommerceApplication.Models
             CartItems = new HashSet<CartItem>();
             OrderLines = new HashSet<OrderLine>(); 
         }
-
+         
         public int ProductId { get; set; }
 
+        [Required, Display (Name = "Product Name")]
         public string ProductName { get; set; }
 
         public string Details { get; set; }
