3922f3c0c9ea61f9aecb217d16d2c5f96e652923
Jeroenouw <jeroenouw@hotmail.com>
2017-05-09 14:24:11 +0200

    tsconfig and components

---
+++ EcommerceApplication/Client/app/components/category/category-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //moduleId: module.id,
+  selector: 'app-category-form',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+  }
+}
